356fbb9eef80bb27571c9b45acd699bfdcc4702c
Nazil1712 <ndhalwala@gmail.com>
2025-08-09 17:11:14 +0530

    For deployment

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ const server = http.createServer(app);
 
 app.use(express.static(path.resolve(__dirname,'dist')))
 
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+// });
+
 app.use(cors());
 app.use(express.json());
 
